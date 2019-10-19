93315d09cee0efbcacd05e7ea0c3643b3a904ca4
Rizky Febrianto Dwiputra <rizkyfebriantodwiputra@gmail.com>
2019-10-19 22:55:02 +0700

    Navigation Component (BottomNavigation)

--- app/src/main/java/com/example/sharingsession/MainActivity.kt
+++ app/src/main/java/com/example/sharingsession/MainActivity.kt
@@ -4,31 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(R.id.first_dest, R.id.second_dest))
-//
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        NavigationUI.setupWithNavController(botNav, navController)
-    }
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(botNav, navController)
 
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp()
+
     }
 }
